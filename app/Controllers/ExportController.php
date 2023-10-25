bbcbb2fc22166463391da37bc3fd252bea0c78a1
LizPerdomo08 <148297425+LizPerdomo08@users.noreply.github.com>
2023-10-25 12:01:24 -0600

    ExportController.php
    Controlador actualizado

--- app/Controllers/ExportController.php
+++ app/Controllers/ExportController.php
@@ -11,35 +11,18 @@ class ExportController extends BaseController
         //
     }
 
-    public function exportDatabase()
+    public function exportarBase()
     {
-        // Ruta donde se guardará el archivo de respaldo
-        $backupFilePath = WRITEPATH . 'logs/' . date('Y-m-d_His') . '.sql';
+        $archivo = 'D:\BD\\2023-10-25_144958.sql'; 
 
-        // Comando mysqldump
-        $command = "\"C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin\\mysqldump.exe\" -u isa -150503 -h localhost blog > $backupFilePath 2> error.log";
-
-
-
-        exec($command, $output, $return); // Agregamos $output y $return
-
-        
-        if ($return !== 0) {
-            // Hubo un error en la ejecución de mysqldump
-            // Puedes imprimir el contenido de $output para obtener más detalles
-            var_dump($output);
-            // Puedes registrar el error en tus registros de aplicación
-            log_message('error', 'Error al ejecutar mysqldump: ' . var_export($output, true));
-        }
-        
-        // Descargar el archivo de respaldo
-        if (file_exists($backupFilePath)) {
+        if (file_exists($archivo)) {
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename="' . basename($backupFilePath) . '"');
-            readfile($backupFilePath);
+            header('Content-Disposition: attachment; filename="' . basename($archivo) . '"');
+            readfile($archivo);
             exit;
+        } else {
+            echo 'La base de datos no fue encontrada';
         }
-        echo 'Proceso terminado!';
     }
 }
